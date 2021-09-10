6db61e1ee0f4dd7d0efa847bcbf93c01194d51fa
Leona Hess <32615985+leonahess@users.noreply.github.com>
2021-09-10 18:26:41 +0200

    rework and move bme688 module

--- smarthome/breakout_garden/README.md
+++ smarthome/breakout_garden/README.md
@@ -25,7 +25,16 @@ INFLUX_BUCKET=smarthome
 INFLUX_ORG=me
 INFLUX_TOKEN=add_your_influx_token_here
 INFLUX_URL=https://influx.leona.pink:8086
-TAG_LOCATION=leona_desk
+
+SGP30_SENSOR=0
+VEML6075_SENSOR=0
+BME688_SENSOR_PRIMARY=0
+BME688_SENSOR_SECONDARY=0
+
+SGP30_SENSOR_TAG_LOCATION=leona_desk
+VEML6075_SENSOR_TAG_LOCATION=leona_desk
+BME688_SENSOR_PRIMARY_TAG_LOCATION=leona_desk1
+BME688_SENSOR_SECONDARY_TAG_LOCATION=leona_desk2
 ```
 
 Install python modules
@@ -35,7 +44,7 @@ pip3 install -r requirements.txt
 
 Move service to /etc/systemd/system and start it
 ```
-sudo cp /home/pi/homelab/smarthome/bme688/bme688.service /etc/systemd/system/
+sudo cp /home/pi/homelab/smarthome/breakout_garden/breakout.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable enviro.service
 sudo systemctl start envrio.service
--- smarthome/breakout_garden/src/breakout.py
+++ smarthome/breakout_garden/src/breakout.py
@@ -5,16 +5,27 @@ from dotenv import Dotenv
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 import bme680
-import time
-
+from sgp30 import SGP30
 ########################################################################################################################
 #                                                                                                                      #
-# Load .env file                                                                                                #
+# Load .env file and load ENVs                                                                                         #
 #                                                                                                                      #
 ########################################################################################################################
 
 dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 os.environ.update(dotenv)
+
+host = socket.gethostname()
+
+sgp30_sensor = os.getenv("SGP30_SENSOR", 0)
+VEML6075_sensor = os.getenv("VEML6075_SENSOR", 0)
+bme688_sensor_primary = os.getenv("BME688_SENSOR_PRIMARY", 0)
+bme688_sensor_secondary = os.getenv("BME688_SENSOR_SECONDARY", 0)
+
+sgp30_sensor_location = os.getenv("SGP30_SENSOR_TAG_LOCATION", "default")
+VEML6075_sensor_location = os.getenv("VEML6075_SENSOR_TAG_LOCATION", "default")
+bme688_sensor_primary_location = os.getenv("BME688_SENSOR_PRIMARY_TAG_LOCATION", "default")
+bme688_sensor_secondary_location = os.getenv("BME688_SENSOR_SECONDARY_TAG_LOCATION", "default")
 
 ########################################################################################################################
 #                                                                                                                      #
@@ -55,47 +66,63 @@ logging.info("Connected to influx: %s" % url)
 #                                                                                                                      #
 ########################################################################################################################
 
-logging.info("Initializing first BME688 sensor")
-sensor1 = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
-logging.info("Initializing second BME688 sensor")
-sensor2 = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+logging.info("Primary BME688 connected: %s" % bme688_sensor_primary)
+if bme688_sensor_primary == 1:
+    logging.info("Initializing first BME688 sensor")
+    sensor1 = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
 
-logging.info("Setting oversampling for first BME688 sensor")
-sensor1.set_humidity_oversample(bme680.OS_2X)
-sensor1.set_pressure_oversample(bme680.OS_4X)
-sensor1.set_temperature_oversample(bme680.OS_8X)
-sensor1.set_filter(bme680.FILTER_SIZE_3)
+    logging.info("Setting oversampling for first BME688 sensor")
+    sensor1.set_humidity_oversample(bme680.OS_2X)
+    sensor1.set_pressure_oversample(bme680.OS_4X)
+    sensor1.set_temperature_oversample(bme680.OS_8X)
+    sensor1.set_filter(bme680.FILTER_SIZE_3)
 
-logging.info("Setting oversampling for second BME688 sensor")
-sensor2.set_humidity_oversample(bme680.OS_2X)
-sensor2.set_pressure_oversample(bme680.OS_4X)
-sensor2.set_temperature_oversample(bme680.OS_8X)
-sensor2.set_filter(bme680.FILTER_SIZE_3)
+logging.info("Secondary BME688 connected: %s" % bme688_sensor_secondary)
+if bme688_sensor_secondary == 1:
+    logging.info("Initializing second BME688 sensor")
+    sensor2 = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+
+    logging.info("Setting oversampling for second BME688 sensor")
+    sensor2.set_humidity_oversample(bme680.OS_2X)
+    sensor2.set_pressure_oversample(bme680.OS_4X)
+    sensor2.set_temperature_oversample(bme680.OS_8X)
+    sensor2.set_filter(bme680.FILTER_SIZE_3)
+
+logging.info("SGP30 connected: %s" % sgp30_sensor)
+if sgp30_sensor == 1:
+    logging.info("Initializing SGP30 sensor")
+    sgp30 = SGP30()
+
+    logging.info("Warming up sensor...")
+    sgp30.start_measurement()
 
 ########################################################################################################################
 #                                                                                                                      #
-# Stuff                                                                                            #
+# Stuff                                                                                                                #
 #                                                                                                                      #
 ########################################################################################################################
 
-host = socket.gethostname()
-location = os.getenv("TAG_LOCATION", "default")
 
-logging.info("Starting main loop")
-
-
-def write_to_influx(measurement, field, field_data, sensor):
-    point = influxdb_client.Point(measurement).tag("host", host).tag("location", location).tag("sensor", sensor).field(field, field_data)
+def write_to_influx(measurement, field, field_data, location_data):
+    point = influxdb_client.Point(measurement).tag("host", host).tag("location", location_data).field(field, field_data)
     write_api.write(bucket=bucket, org=org, record=point)
 
 
+logging.info("Starting main loop")
 while True:
-    if sensor1.get_sensor_data():
-        write_to_influx("temperature", "temperature", float(sensor1.data.temperature),1)
-        write_to_influx("pressure", "pressure", float(sensor1.data.pressure), 1)
-        write_to_influx("humidity", "humidity", float(sensor1.data.humidity), 1)
+    if bme688_sensor_primary == 1:
+        if sensor1.get_sensor_data():
+            write_to_influx("temperature", "temperature", float(sensor1.data.temperature), bme688_sensor_primary_location)
+            write_to_influx("pressure", "pressure", float(sensor1.data.pressure), bme688_sensor_primary_location)
+            write_to_influx("humidity", "humidity", float(sensor1.data.humidity), bme688_sensor_primary_location)
 
-    if sensor2.get_sensor_data():
-        write_to_influx("temperature", "temperature", float(sensor2.data.temperature), 2)
-        write_to_influx("pressure", "pressure", float(sensor2.data.pressure), 2)
-        write_to_influx("humidity", "humidity", float(sensor2.data.humidity), 2)
+    if bme688_sensor_secondary == 1:
+        if sensor2.get_sensor_data():
+            write_to_influx("temperature", "temperature", float(sensor2.data.temperature), bme688_sensor_secondary_location)
+            write_to_influx("pressure", "pressure", float(sensor2.data.pressure), bme688_sensor_secondary_location)
+            write_to_influx("humidity", "humidity", float(sensor2.data.humidity), bme688_sensor_secondary_location)
+
+    if sgp30_sensor == 1:
+        eco2, tvoc = sgp30.get_air_quality()
+        write_to_influx("gas", "eco2", int(eco2), sgp30_sensor_location)
+        write_to_influx("gas", "tvoc", int(tvoc), sgp30_sensor_location)
