a28b02ce062ab6f9859aa8dd837bc7566ca04866
Leona Hess <32615985+leonahess@users.noreply.github.com>
2021-09-10 17:47:44 +0200

    remove makeshift aqi stuff again from bme688 module

--- smarthome/bme688/src/bme688.py
+++ smarthome/bme688/src/bme688.py
@@ -72,16 +72,6 @@ sensor2.set_pressure_oversample(bme680.OS_4X)
 sensor2.set_temperature_oversample(bme680.OS_8X)
 sensor2.set_filter(bme680.FILTER_SIZE_3)
 
-sensor2.set_gas_heater_temperature(320)
-sensor2.set_gas_heater_duration(150)
-sensor2.select_gas_heater_profile(0)
-
-start_time = time.time()
-curr_time = time.time()
-burn_in_time = 300
-
-burn_in_data = []
-
 ########################################################################################################################
 #                                                                                                                      #
 # Stuff                                                                                            #
@@ -99,24 +89,6 @@ def write_to_influx(measurement, field, field_data, sensor):
     write_api.write(bucket=bucket, org=org, record=point)
 
 
-logging.info("Collecting gas resistance burn-in data for 5 mins")
-while curr_time - start_time < burn_in_time:
-    curr_time = time.time()
-    if sensor2.get_sensor_data() and sensor2.data.heat_stable:
-        gas = sensor2.data.gas_resistance
-        burn_in_data.append(gas)
-        logging.info("Gas: {0} Ohms".format(gas))
-        time.sleep(1)
-
-gas_baseline = sum(burn_in_data[-50:]) / 50.0
-
-# Set the humidity baseline to 40%, an optimal indoor humidity.
-hum_baseline = 40.0
-
-# This sets the balance between humidity and gas reading in the
-# calculation of air_quality_score (25:75, humidity:gas)
-hum_weighting = 0.25
-
 while True:
     if sensor1.get_sensor_data():
         write_to_influx("temperature", "temperature", float(sensor1.data.temperature),1)
@@ -127,36 +99,3 @@ while True:
         write_to_influx("temperature", "temperature", float(sensor2.data.temperature), 2)
         write_to_influx("pressure", "pressure", float(sensor2.data.pressure), 2)
         write_to_influx("humidity", "humidity", float(sensor2.data.humidity), 2)
-
-    if sensor2.get_sensor_data() and sensor2.data.heat_stable:
-        gas = sensor2.data.gas_resistance
-        gas_offset = gas_baseline - gas
-
-        hum = sensor2.data.humidity
-        hum_offset = hum - hum_baseline
-
-        # Calculate hum_score as the distance from the hum_baseline.
-        if hum_offset > 0:
-            hum_score = (100 - hum_baseline - hum_offset)
-            hum_score /= (100 - hum_baseline)
-            hum_score *= (hum_weighting * 100)
-
-        else:
-            hum_score = (hum_baseline + hum_offset)
-            hum_score /= hum_baseline
-            hum_score *= (hum_weighting * 100)
-
-        # Calculate gas_score as the distance from the gas_baseline.
-        if gas_offset > 0:
-            gas_score = (gas / gas_baseline)
-            gas_score *= (100 - (hum_weighting * 100))
-
-        else:
-            gas_score = 100 - (hum_weighting * 100)
-
-        # Calculate air_quality_score.
-        air_quality_score = hum_score + gas_score
-        write_to_influx("Air Quality Index", "AQI", float(air_quality_score), 2)
-
-        time.sleep(1)
-
