4fc63d95a24f7a0a9be8eb1b43174f9f98c69cb0
L <32615985+leonahess@users.noreply.github.com>
2021-09-24 16:22:39 +0200

    Update update-plex.sh

--- lxc/211-plex/crons/update-plex.sh
+++ lxc/211-plex/crons/update-plex.sh
@@ -3,3 +3,4 @@
 docker-compose -f /root/homelab/lxc/211-plex/plex.yml pull
 docker-compose -f /root/homelab/lxc/211-plex/plex.yml down
 docker-compose -f /root/homelab/lxc/211-plex/plex.yml up -d
+docker image prune -f
