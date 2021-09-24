68c98560269792cf9603a2dfa0df543560f42182
L <32615985+leonahess@users.noreply.github.com>
2021-09-24 16:21:50 +0200

    Update update-nextcloud.sh

--- lxc/210-nextcloud/crons/update-nextcloud.sh
+++ lxc/210-nextcloud/crons/update-nextcloud.sh
@@ -3,3 +3,4 @@
 docker-compose -f /root/homelab/lxc/210-nextcloud/nextcloud.yml pull
 docker-compose -f /root/homelab/lxc/210-nextcloud/nextcloud.yml down
 docker-compose -f /root/homelab/lxc/210-nextcloud/nextcloud.yml up -d
+docker image prune -f
