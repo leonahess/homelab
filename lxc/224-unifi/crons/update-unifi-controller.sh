19d193d2940d76772a2389a206390aedcc6d4c3e
L <32615985+leonahess@users.noreply.github.com>
2021-09-24 16:23:08 +0200

    Update update-unifi-controller.sh

--- lxc/224-unifi/crons/update-unifi-controller.sh
+++ lxc/224-unifi/crons/update-unifi-controller.sh
@@ -3,3 +3,4 @@
 docker-compose -f /root/homelab/lxc/224-unifi/unifi-controller.yml pull
 docker-compose -f /root/homelab/lxc/224-unifi/unifi-controller.yml down
 docker-compose -f /root/homelab/lxc/224-unifi/unifi-controller.yml up -d
+docker image prune -f
