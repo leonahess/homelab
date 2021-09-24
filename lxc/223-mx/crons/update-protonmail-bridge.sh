408b06e3f5fac84e7ca4f2cd326699638b9b6780
L <32615985+leonahess@users.noreply.github.com>
2021-09-24 16:22:55 +0200

    Update update-protonmail-bridge.sh

--- lxc/223-mx/crons/update-protonmail-bridge.sh
+++ lxc/223-mx/crons/update-protonmail-bridge.sh
@@ -3,3 +3,4 @@
 docker-compose -f /root/homelab/lxc/223-mx/protonmail-bridge.yml pull
 docker-compose -f /root/homelab/lxc/223-mx/protonmail-bridge.yml down
 docker-compose -f /root/homelab/lxc/223-mx/protonmail-bridge.yml up -d
+docker image prune -f
