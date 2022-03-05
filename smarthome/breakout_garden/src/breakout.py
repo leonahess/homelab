18b013a79db6da74b9e9b960a3617ab6c77ad29f
Leona Hess <32615985+leonahess@users.noreply.github.com>
2022-03-05 18:14:38 +0100

    add VEML6075 sensor to breakout script

--- smarthome/breakout_garden/src/breakout.py
+++ smarthome/breakout_garden/src/breakout.py
@@ -3,6 +3,8 @@ import os
 import socket
 import time
 import bme680
+import veml6075
+import smbus2
 
 from dotenv import Dotenv
 from prometheus_client import start_http_server, Summary, Gauge
@@ -70,6 +72,15 @@ if bme688_sensor_secondary == "1":
     sensor2.set_temperature_oversample(bme680.OS_8X)
     sensor2.set_filter(bme680.FILTER_SIZE_3)
 
+logging.info("VEML6075 connected: %s" % VEML6075_sensor)
+if VEML6075_sensor == "1":
+    logging.info("Initializing VEML6075 sensor")
+    bus = smbus2.SMBus(1)
+    uv_sensor = veml6075.VEML6075(i2c_dev=bus)
+    uv_sensor.set_shutdown(False)
+    uv_sensor.set_high_dynamic_range(False)
+    uv_sensor.set_integration_time('100ms')
+
 logging.info("SGP30 connected: %s" % sgp30_sensor)
 if sgp30_sensor == "1":
     logging.info("Initializing SGP30 sensor")
@@ -87,6 +98,7 @@ if sgp30_sensor == "1":
 LOCATION1 = os.getenv("BME688_SENSOR_PRIMARY_TAG_LOCATION", "default")
 LOCATION2 = os.getenv("BME688_SENSOR_SECONDARY_TAG_LOCATION", "default")
 SGP_LOCATION = os.getenv("SGP30_SENSOR_TAG_LOCATION", "default")
+VEML6075_LOCATION = os.getenv("VEML6075_SENSOR_TAG_LOCATION", "default")
 ROOM = os.getenv("ROOM", "default")
 
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
@@ -96,7 +108,9 @@ pressure = Gauge('smarthome_pressure_hectopascal', 'Pressure in percents provide
 hum = Gauge('smarthome_humidity_percent', 'Humidity in percents provided by the sensor', ['location', 'room'])
 eco2 = Gauge('smarthome_eco2_ppm', 'equivalent CO2 concentration in ppm, provided by the sensor', ['location', 'room'])
 tvoc = Gauge('smarthome_tvoc_ppb', 'TVOC concentration in ppb, provided by the sensor', ['location', 'room'])
-
+uva_ind = Gauge('smarthome_uva_index', 'UVA Index, provided by the VEML6075 sensor', ['location', 'room'])
+uvb_ind = Gauge('smarthome_uvb_index', 'UVB Index, provided by the VEML6075 sensor', ['location', 'room'])
+uvavg_ind = Gauge('smarthome_avg_uv_index', 'Average UV Index, provided by the VEML6075 sensor', ['location', 'room'])
 
 @REQUEST_TIME.time()
 def process_request():
@@ -111,6 +125,14 @@ def process_request():
             temp.labels(location=LOCATION2, room=ROOM).set(float(sensor2.data.temperature))
             pressure.labels(location=LOCATION2, room=ROOM).set(float(sensor2.data.pressure))
             hum.labels(location=LOCATION2, room=ROOM).set(float(sensor2.data.humidity))
+
+    if VEML6075_sensor == "1":
+        uva, uvb = uv_sensor.get_measurements()
+        uv_comp1, uv_comp2 = uv_sensor.get_comparitor_readings()
+        uv_indices = uv_sensor.convert_to_index(uva, uvb, uv_comp1, uv_comp2)
+        uva_ind.labels(location=VEML6075_LOCATION, room=ROOM).set(float(uv_indices[0]))
+        uvb_ind.labels(location=VEML6075_LOCATION, room=ROOM).set(float(uv_indices[1]))
+        uvavg_ind.labels(location=VEML6075_LOCATION, room=ROOM).set(float(uv_indices[2]))
 
     if sgp30_sensor == "1":
         res = sgp30.get_air_quality()
