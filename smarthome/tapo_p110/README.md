30413d322aeb206a0f3339e7029da79f0383c40d
Leona Hess <32615985+leonahess@users.noreply.github.com>
2022-12-28 17:39:55 +0100

    Add Tapo P110

---
+++ smarthome/tapo_p110/README.md
@@ -0,0 +1,3 @@
+# Prometheus exporter to read TP-Link Tapo P110 smart plugs
+
+- https://github.com/fishbigger/TapoP100
---
+++ smarthome/tapo_p110/src/tapo.py
@@ -0,0 +1,62 @@
+import logging
+import os
+import time
+from prometheus_client import start_http_server, Summary, Gauge
+from PyP100 import PyP110
+
+########################################################################################################################
+#                                                                                                                      #
+# Logging configuration                                                                                                #
+#                                                                                                                      #
+########################################################################################################################
+
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+########################################################################################################################
+#                                                                                                                      #
+# Initialize Sensors                                                                                                   #
+#                                                                                                                      #
+########################################################################################################################
+
+IP = os.getenv("IP", "default")
+USERNAME = os.getenv("USERNAME", "default")
+PASSWORD = os.getenv("PASSWORD", "default")
+
+logging.info("Initializing Plug with IP: %s" % IP)
+p110 = PyP110.P110(IP, USERNAME, PASSWORD)
+
+p110.handshake() #Creates the cookies required for further methods
+p110.login() #Sends credentials to the plug and creates AES Key and IV for further methods
+
+########################################################################################################################
+#                                                                                                                      #
+# Stuff                                                                                            #
+#                                                                                                                      #
+########################################################################################################################
+
+
+LOCATION = os.getenv("LOCATION", "default")
+ROOM = os.getenv("ROOM", "default")
+
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+power = Gauge('smarthome_power_milliwat', 'Power in milliwat provided by the plug', ['location', 'room'])
+
+
+@REQUEST_TIME.time()
+def process_request():
+  response = p110.getEnergyUsage()
+  logging.info(response)
+
+
+if __name__ == '__main__':
+    start_http_server(8000)
+
+    while True:
+        process_request()
+        time.sleep(5)
+
+
