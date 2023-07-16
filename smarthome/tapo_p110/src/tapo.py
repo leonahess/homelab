1d0d20062c14ac8f339798517e709ec3b78ec9c8
root <root@prometheus.leona.pink>
2023-07-16 11:48:11 +0000

    update prometheus stuff

--- smarthome/tapo_p110/src/tapo.py
+++ smarthome/tapo_p110/src/tapo.py
@@ -42,25 +42,26 @@ LOCATION = os.getenv("LOCATION", "default")
 ROOM = os.getenv("ROOM", "default")
 DEVICE = os.getenv("DEVICE", "default")
 IS_LIGHT = os.getenv("IS_LIGHT", "default")
+TYPE = os.getenv("TYPE", "default")
 
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
-power = Gauge('smarthome_power_milliwatt', 'Power in milliwatt provided by the plug', ['location', 'room', 'device', 'is_light'])
-energy_today = Gauge('smarthome_energy_today_watthours', 'Todays energy in watthours provided by the plug', ['location', 'room', 'device', 'is_light'])
-energy_month = Gauge('smarthome_energy_month_watthours', 'This months energy in watthours provided by the plug', ['location', 'room', 'device', 'is_light'])
-runtime_today = Gauge('smarthome_runtime_today_minutes', 'Todays runtime in minutes provided by the plug', ['location', 'room', 'device', 'is_light'])
-runtime_month = Gauge('smarthome_runtime_month_minutes', 'Todays runtime in minutes provided by the plug', ['location', 'room', 'device', 'is_light'])
+power = Gauge('smarthome_power_milliwatt', 'Power in milliwatt provided by the plug', ['location', 'room', 'device', 'is_light', 'type'])
+energy_today = Gauge('smarthome_energy_today_watthours', 'Todays energy in watthours provided by the plug', ['location', 'room', 'device', 'is_light', 'type'])
+energy_month = Gauge('smarthome_energy_month_watthours', 'This months energy in watthours provided by the plug', ['location', 'room', 'device', 'is_light', 'type'])
+runtime_today = Gauge('smarthome_runtime_today_minutes', 'Todays runtime in minutes provided by the plug', ['location', 'room', 'device', 'is_light', 'type'])
+runtime_month = Gauge('smarthome_runtime_month_minutes', 'Todays runtime in minutes provided by the plug', ['location', 'room', 'device', 'is_light', 'type'])
 
 @REQUEST_TIME.time()
 def process_request():
   response = p110.getEnergyUsage()
   logging.debug(response)
 
-  power.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT).set(response["result"]["current_power"])
-  energy_today.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT).set(response["result"]["today_energy"])
-  energy_month.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT).set(response["result"]["month_energy"])
-  runtime_today.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT).set(response["result"]["today_runtime"])
-  runtime_month.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT).set(response["result"]["month_runtime"])
+  power.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT, type=TYPE).set(response["result"]["current_power"])
+  energy_today.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT, type=TYPE).set(response["result"]["today_energy"])
+  energy_month.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT, type=TYPE).set(response["result"]["month_energy"])
+  runtime_today.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT, type=TYPE).set(response["result"]["today_runtime"])
+  runtime_month.labels(location=LOCATION, room=ROOM, device=DEVICE, is_light=IS_LIGHT, type=TYPE).set(response["result"]["month_runtime"])
 
 if __name__ == '__main__':
     start_http_server(8000)
