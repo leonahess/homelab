0b45168bcaed5374836069358aa3779b42e735fa
Leona Hess <32615985+leonahess@users.noreply.github.com>
2023-09-18 21:09:37 +0200

    add nginx proxy to prom lxc

--- lxc/226-prometheus/crons/certbot.sh
+++ lxc/226-prometheus/crons/certbot.sh
@@ -6,23 +6,23 @@ certbot certonly --dns-cloudflare --dns-cloudflare-credentials ~/.secrets/cloudf
 
 docker-compose -f /root/homelab/lxc/226-prometheus/docker-compose.yml down
 
-rm /mnt/appdata/prometheus/fullchain.pem
-rm /mnt/appdata/prometheus/privkey.pem
+rm /mnt/appdata/nginx/prometheus/fullchain.pem
+rm /mnt/appdata/nginx/prometheus/privkey.pem
 
-rm /mnt/appdata/alertmanager/fullchain.pem
-rm /mnt/appdata/alertmanager/privkey.pem
+rm /mnt/appdata/nginx/alertmanager/fullchain.pem
+rm /mnt/appdata/nginx/alertmanager/privkey.pem
 
-cp /etc/letsencrypt/live/prometheus.leona.pink/fullchain.pem /mnt/appdata/prometheus/fullchain.pem
-cp /etc/letsencrypt/live/prometheus.leona.pink/privkey.pem /mnt/appdata/prometheus/privkey.pem
+cp /etc/letsencrypt/live/prometheus.leona.pink/fullchain.pem /mnt/appdata/nginx/prometheus/fullchain.pem
+cp /etc/letsencrypt/live/prometheus.leona.pink/privkey.pem /mnt/appdata/nginx/prometheus/privkey.pem
 
-cp /etc/letsencrypt/live/alertmanager.leona.pink/fullchain.pem /mnt/appdata/alertmanager/fullchain.pem
-cp /etc/letsencrypt/live/alertmanager.leona.pink/privkey.pem /mnt/appdata/alertmanager/privkey.pem
+cp /etc/letsencrypt/live/alertmanager.leona.pink/fullchain.pem /mnt/appdata/nginx/alertmanager/fullchain.pem
+cp /etc/letsencrypt/live/alertmanager.leona.pink/privkey.pem /mnt/appdata/nginx/alertmanager/privkey.pem
 
-chown 472 /mnt/appdata/prometheus/fullchain.pem
-chown 472 /mnt/appdata/prometheus/privkey.pem
+chown 472 /mnt/appdata/nginx/prometheus/fullchain.pem
+chown 472 /mnt/appdata/nginx/prometheus/privkey.pem
 
-chown 472 /mnt/appdata/alertmanager/fullchain.pem
-chown 472 /mnt/appdata/alertmanager/privkey.pem
+chown 472 /mnt/appdata/nginx/alertmanager/fullchain.pem
+chown 472 /mnt/appdata/nginx/alertmanager/privkey.pem
 
 
 docker-compose -f /root/homelab/lxc/222-grafana/docker-compose.yml up -d
